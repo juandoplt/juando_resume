b76c394a72c2a643bbfa1bc0ac6e1214eb12e040
JuandoPLT <45502036+juandoplt@users.noreply.github.com>
2022-03-31 11:13:59 -0300

    A

--- src/views/Portfolio/fpd/Show/ShowSection.js
+++ src/views/Portfolio/fpd/Show/ShowSection.js
@@ -1,11 +1,12 @@
 import Separator from "../../../../components/general/Separator";
 
 import {
-    SliderContainer,
-    SliderTitle,
-    SwiperImage,
-    SwiperContent,
-    SwiperTextContent
+    ShowContainer,
+    ShowTitle,
+    ShowImage,
+    ShowContent,
+    ShowVideoContent,
+    ShowContentDescription
 } from "./ShowSection.styles";
 import Parallax from '../../../../components/general/ParrallaxImages';
 
@@ -14,53 +15,18 @@ const ShowSection = () => {
     return (
         <>
             <Separator fill="#efefef" backgroundColor="#010101" translateY="-1px" />
-            <SliderContainer>
-                <SliderTitle>Shows</SliderTitle>
-                {/* {images[0].mockups.map((item, i) => {
-                        return (
-                            <SwiperSlide key={i} >
-                                <SwiperImage src={`${process.env.PUBLIC_URL + '/img/portfolio/fpd/' + item}`} />
-                            </SwiperSlide>
-                        )
-                    })} */}
-                <Parallax customClass="parallax-video">
-                    <SwiperContent className="noselect">
-                        <iframe src="https://www.youtube.com/embed/NIKdwoldzIA" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                        <SwiperTextContent>
-                            <h1 style={{ color: "#fff", textTransform: "uppercase" }}>El ping pong de la China</h1>
-                            <p style={{ color: "#fff" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </SwiperTextContent>
-                    </SwiperContent>
-                </Parallax>
-                <Parallax customClass="parallax-video">
-                    <SwiperContent className="noselect">
-                        <iframe src="https://www.youtube.com/embed/KZOQGWjlCp4" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                        <SwiperTextContent>
-                            <h1 style={{ color: "#fff", textTransform: "uppercase" }}>Modo hincha - Antonio Serpa</h1>
-                            <p style={{ color: "#fff" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </SwiperTextContent>
-                    </SwiperContent>
-                </Parallax>
-                <Parallax customClass="parallax-video">
-                    <SwiperContent className="noselect">
-                        <iframe src="https://www.youtube.com/embed/KSsuRn3DM68" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                        <SwiperTextContent>
-
-                            <h1 style={{ color: "#fff", textTransform: "uppercase" }}>Modo hincha - Ernesto Provitilo</h1>
-                            <p style={{ color: "#fff" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </SwiperTextContent>
-                    </SwiperContent>
-                </Parallax>
-                <Parallax customClass="parallax-video">
-                    <SwiperContent className="noselect">
-                        <iframe src="https://www.youtube.com/embed/6_3s3T7pORA" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                        <SwiperTextContent>
-                            <h1 style={{ color: "#fff", textTransform: "uppercase" }}>Lo mejor de la China</h1>
-                            <p style={{ color: "#fff" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </SwiperTextContent>
-                    </SwiperContent>
-                </Parallax>
-            </SliderContainer>
+            <ShowContainer>
+                <ShowTitle>Shows</ShowTitle>
+                    <ShowVideoContent>
+                        <img src={`${process.env.PUBLIC_URL + '/img/portfolio/fpd/thumb_0.jpg'}`}/>
+                    </ShowVideoContent>
+                <ShowContent>
+                        <ShowImage src={`${process.env.PUBLIC_URL + '/img/portfolio/fpd/thumb_1.jpg'}`} />
+                        <ShowImage src={`${process.env.PUBLIC_URL + '/img/portfolio/fpd/thumb_2.jpg'}`} />
+                        <ShowImage src={`${process.env.PUBLIC_URL + '/img/portfolio/fpd/thumb_3.jpg'}`} />
+                        <ShowImage src={`${process.env.PUBLIC_URL + '/img/portfolio/fpd/thumb_4.jpg'}`} />
+                </ShowContent>
+            </ShowContainer>
         </>
     )
 }
--- src/views/Portfolio/fpd/Show/ShowSection.styles.js
+++ src/views/Portfolio/fpd/Show/ShowSection.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const SliderContainer = styled.div`
+const ShowContainer = styled.div`
   /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#000000+0,212121+100 */
   background: rgb(0,0,0); /* Old browsers */
   background: -moz-linear-gradient(top,  rgba(0,0,0,1) 0%, rgba(33,33,33,1) 100%); /* FF3.6-15 */
@@ -8,52 +8,19 @@ const SliderContainer = styled.div`
   background: linear-gradient(to bottom,  rgba(0,0,0,1) 0%,rgba(33,33,33,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#212121',GradientType=0 ); /* IE6-9 */
   padding-top: 10%;
-  padding-bottom: 25%;
+  padding-bottom: 15%;
+  // height:100vh;
   overflow:hidden;
+  .parallax-video{
+    margin:0 .3rem
+  }
   @media (min-width: 576px) { 
     padding-top: 5%;
-    padding-bottom: 15%;
-  }
-  .parallax-video{
-     position:relative;
-     height:100%;
-     width:100%;
-     padding:5%;
-     margin:2% 0;
-     background-color:black;
-     @media (min-width: 576px) { 
-       margin:10% 0;
-       padding:0%;
-       width:60%;
-    }
-  }
-  .parallax-video:nth-child(2){
-      left:auto;
-      @media (min-width: 576px) { 
-        left:4%;
-      }
-  }
-  .parallax-video:nth-child(3){
-      left:auto;
-      @media (min-width: 576px) { 
-        left:30%;
-      }
-  }
-  .parallax-video:nth-child(4){
-      left:auto;
-      @media (min-width: 576px) { 
-        left:12%;
-      }
-  }
-  .parallax-video:nth-child(5){
-      left:auto;
-      @media (min-width: 576px) { 
-        left:38%;
-      }
+    padding-bottom: 10%;
   }
 `
 
-const SliderTitle = styled.h1`
+const ShowTitle = styled.h1`
   text-align: center; 
   color: #fff;
   width:100%;
@@ -65,46 +32,51 @@ const SliderTitle = styled.h1`
   }
 `
 
-const SwiperContent = styled.div`
-  margin: 0 auto;
+const ShowContent = styled.div`
   display:flex;
-  flex-direction:column;
   justify-content:center;
-  align-items:center;
-  iframe{
-    height:300px;
-    width:100%;
-  }
-
+  padding:2rem 0;
+  flex-wrap:wrap;
   @media (min-width: 576px) { 
     flex-direction:row;
-    iframe{
-      height:440px;
+  }
+`
+
+const ShowVideoContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding:0.5rem;
+  img{
+    width:100%;
+    @media (min-width: 576px) { 
+      width:80%;
+    }
+    @media (min-width: 1500px) { 
+      width:60%;
     }
   }
+  
 `
 
-const SwiperTextContent = styled.div`
-  padding: 15px;
-  width: 100%;
-  @media (min-width: 576px) { 
-    width: 70%;
-  }
+const ShowContentDescription = styled.div`
 `
 
-const SwiperImage = styled.img`
-  max-height: 500px;
-  margin: 0 auto;
+const ShowImage = styled.img`
+  width:150px;
   display: block;
+  margin:0 .5rem;
+  margin-bottom:.5rem;
   @media (min-width: 576px) { 
-    max-height: 700px;
+    width:250px;
   }
 `
 
 export {
-  SliderContainer,
-  SliderTitle,
-  SwiperImage,
-  SwiperContent,
-  SwiperTextContent
+  ShowContainer,
+  ShowTitle,
+  ShowImage,
+  ShowContent,
+  ShowContentDescription,
+  ShowVideoContent
 };
